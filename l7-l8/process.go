63896410fcdf9d8c228cef4c26c0e9d58ef29363
Abigail Mureithi <amureith@bu.edu>
2026-07-14 15:16:38 -0400

    implemented compare and tested functions

--- l7-l8/process.go
+++ l7-l8/process.go
@@ -1,22 +1,21 @@
 package main
 
-
 type Process struct {
 	clock []int
-	pid int
-	N int
+	pid   int
+	N     int
 }
 
-func (p *Process) Start(N int, PID int)  {
+func (p *Process) Start(N int, PID int) {
 	p.N = N
 	p.pid = PID
 	p.clock = make([]int, N)
-	
+
 }
 
 func (p *Process) Internal() {
 	p.clock[p.pid] += 1
-	
+
 }
 
 func (p *Process) Send() []int {
@@ -38,5 +37,24 @@ func (p *Process) Receive(ts []int) {
 }
 
 func Compare(ts1 []int, ts2 []int) int {
+	ts1LessThanTs2 := false
+	ts2LessThanTs1 := false
+
+	for i := 0; i < len(ts1); i++ {
+		if ts1[i] < ts2[i] {
+			ts1LessThanTs2 = true
+		} else if ts1[i] > ts2[i] {
+			ts2LessThanTs1 = true
+		}
+	}
+
+	if ts1LessThanTs2 == true && !ts2LessThanTs1 {
+		return -1
+	}
+
+	if ts2LessThanTs1 == true && !ts1LessThanTs2 {
+		return 1
+	}
+
 	return 0
 }
